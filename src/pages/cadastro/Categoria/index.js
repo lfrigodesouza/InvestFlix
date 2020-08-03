0a6ab36e2ea83330c1d95a21ba3a0f62228e4b54
Lucas Frigo de Souza <lucas.iteris@easynvest.com.br>
2020-08-03 14:59:51 -0300

    Cria json-server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
@@ -24,6 +24,15 @@ function CadastroCategoria() {
     const { value } = evt.target;
     setValue(evt.target.getAttribute('name'), value);
   }
+
+  useEffect(() =>{
+    const URL = 'http://localhost:8080/categorias';
+    fetch(URL)
+      .then(async (res) => {
+        const response = await res.json();
+        setCategorias([...response]);
+      });
+  }, []);
 
   return (
     <PageDefault>
@@ -67,6 +76,11 @@ function CadastroCategoria() {
         <Button>Cadastrar</Button>
       </form>
 
+      {categorias.length === 0 && (
+        <div>
+          Carregando...
+        </div>
+      )}
       <ul>
         {categorias.map((categoria) => <li key={`${categoria.nome}`}>{categoria.nome}</li>)}
       </ul>
